fb2490c66775ab7ba1d14f9b30c300ce86a432e9
Shukl-amber <shukl.ember@gmail.com>
2025-04-10 00:29:59 +0530

    modified:   CPP/CP(CPP)/Outside_Arr.cpp

--- CPP/CP(CPP)/Outside_Arr.cpp
+++ CPP/CP(CPP)/Outside_Arr.cpp
@@ -11,48 +11,53 @@ int main()
     {
         int size;
         cin >> size;
-
         vector<ll> arr(size);
         unordered_set<ll> numbers;
 
         for (int i = 0; i < size; i++)
-            cin >> arr[i],
-                numbers.insert(arr[i]);
-
-        if (size == 1)
-            if (numbers.count(0))
-                cout << "-1" << endl;
-            else
-            {
-                cout << arr[0] << " " << arr[0] << endl;
-                continue;
-            }
-
-        vector<ll> Arr;
-        int set = min(5, size);
-
-        for (int i = 0; i < set; i++)
-            Arr.push_back(arr[i]);
-        for (int i = max(0, size - set); i < size; i++)
-            Arr.push_back(arr[i]);
-
-        bool sol = false;
-        for (int i = 0; i < Arr.size() && !sol; i++)
         {
-            for (int j = i; j < Arr.size() && !sol; j++)
-            {
-                ll sum = Arr[i] + Arr[j];
-                if (numbers.count(sum) == 0)
-                {
-                    cout << Arr[i] << " " << Arr[j] << endl;
-                    sol = true;
-                }
-            }
+            cin >> arr[i];
+            numbers.insert(arr[i]);
         }
 
-        if (!sol)
-            cout << "-1" << endl;
-    }
+        if (size == 1)
+        {
+            ll sum = arr[0] * 2;
+            if (numbers.count(sum))
+                cout << -1 << endl;
+            else
+                cout << arr[0] << " " << arr[0] << "\n";
+            continue;
+        }
 
-    return 0;
+        vector<ll> candidates;
+        unordered_set<ll> added;
+
+        for (int i = 0; i < min(5, size); i++)
+        {
+            if (!added.count(arr[i]))
+                candidates.push_back(arr[i]),
+                    added.insert(arr[i]);
+
+            for (int i = max(0, size - 5); i < size; i++)
+                if (!added.count(arr[i]))
+                    candidates.push_back(arr[i]),
+                        added.insert(arr[i]);
+
+            bool found = false;
+            for (int i = 0; i < candidates.size() && !found; i++)
+                for (int j = 0; j < candidates.size() && !found; j++)
+                {
+                    ll sum = candidates[i] + candidates[j];
+                    if (!numbers.count(sum))
+                        cout << candidates[i] << " " << candidates[j] << "\n",
+                        found = true;
+                }
+
+            if (!found)
+                cout << -1 << endl;
+        }
+
+        return 0;
+    }
 }
