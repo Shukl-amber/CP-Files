f2ab047025eaa531ad4b1244a3c6ca6effe0abb2
Shukl-amber <shukl.ember@gmail.com>
2025-04-23 22:10:17 +0530

    modified:   CP(CPP)/S_to_T.cpp

--- CPP/CP(CPP)/S_to_T.cpp
+++ CPP/CP(CPP)/S_to_T.cpp
@@ -1,56 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-int main(void)
-{
-    int tc;
-    cin >> tc;
-    while (tc--)
-    {
-        string s, t;
-        ll size;
-        cin >> size;
-        cin >> s;
-        cin >> t;
 
-        if (s == t)
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int TC;
+    cin >> TC;
+    while (TC--)
+    {
+        int N;
+        string s, t;
+        cin >> N >> s >> t;
+
+        if (s[0] != t[0])
         {
-            cout << 0 << endl;
+            cout << -1 << "\n";
             continue;
         }
-        else if (s[0] != t[0])
+
+        vector<int> ops;
+        ops.reserve(3 * N);
+
+        for (int i = N - 1; i >= 1; --i)
         {
-            cout << -1 << endl;
-            continue;
-        }
-        ll zcount = 0, diffs = 0;
-        vector<int> arr;
-        for (ll i = 0; i < size; i++)
-        {
-            if (s[i] == '0')
+            if (s[i] == t[i])
+                continue;
+
+            if (s[i - 1] == '1')
             {
-                zcount++;
+                ops.push_back(i);
+                s[i] = (s[i] == '0' ? '1' : '0');
             }
-            if (s[i] != t[i])
+            else
             {
-                diffs++;
-                arr.push_back(i);
+                if (i - 2 < 0 || s[i - 2] == '0')
+                {
+                    ops.clear();
+                    break;
+                }
+                ops.push_back(i - 1);
+                s[i - 1] = '1';
+                ops.push_back(i);
+                s[i] = (s[i] == '0' ? '1' : '0');
+                ops.push_back(i - 1);
+                s[i - 1] = '0';
             }
         }
-        if (zcount == size)
+
+        if (ops.empty() && s != t)
         {
-            cout << -1 << endl;
+            cout << -1 << "\n";
             continue;
         }
-        if (zcount == 0)
-        {
-            cout << diffs << endl;
-            for (auto i : arr)
-            {
-                cout << i << " ";
-            }
-            continue;
-        }
+
+        cout << ops.size() << "\n";
+        if (!ops.empty())
+            for (int x : ops)
+                cout << x << " ";
+
+        cout << "\n";
     }
     return 0;
 }
