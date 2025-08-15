9eedb3e42afb8543fb6c089f9029c234e3687526
Shukl-amber <shukl.ember@gmail.com>
2025-08-15 22:54:17 +0530

    new file:   CPP/CP(CPP)/Q3.cpp

---
+++ CPP/CP(CPP)/Q3.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        ll size;
+        cin >> size;
+        ll fences[size], icount = 0, mins = 0, largest = 0, map;
+        unordered_map<ll, ll> freq;
+
+        cout << freq[1] << endl;
+
+        // for (ll i = 0; i < size; i++)
+        // {
+        //     cin >> fences[i];
+        //     if (fences[i] == 1)
+        //         icount++;
+        //     else
+        //         freq[fences[i]]++;
+        // }
+
+        // for (auto i : freq)
+        //     if (i.second > largest)
+        //         largest = i.second;
+
+        // if (largest > icount)
+        // {
+        //     mins = 1;
+        //     mins += (size - largest);
+        // }
+        // else
+        //     mins += (size - icount);
+
+        // cout << mins << endl;
+    }
+}
