9be7a104860fe0bb9f7cf77ab75cb12fb5cd84ee
Amber Shukla <ember.shukl@gmail.com>
2025-08-28 23:08:07 +0530

    .

--- CPP/coin_change.cpp
+++ CPP/coin_change.cpp
@@ -1,35 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+ll coin_1 = 3, coin_2 = 5, coin_3 = 7;
 vector<ll> minCoins;
 
 ll solve(ll amt)
 {
-    if (amt <= 0)
-        return 0;
-    if (amt <= 3)
-        return amt;
+    if (amt < 0)
+        return INT_MAX;
 
-    return min({solve(amt - 1), solve(amt - 2), solve(amt - 3)}) + 1;
+    if (amt == 0)
+        return 0;
+
+    if (minCoins[amt] != -1)
+        return minCoins[amt];
+    
+    ll minResult = min({solve(amt - coin_1), solve(amt - coin_2), solve(amt - coin_3)});
+    
+    if (minResult == INT_MAX) {
+        minCoins[amt] = INT_MAX;
+        return INT_MAX;
+    } else {
+        minCoins[amt] = minResult + 1;
+        return minResult + 1;
+    }
 }
 
 int main()
 {
-    // ll size;
-    // cout << "Enter Number of Different Coins: ";
-    // cin >> size;
-    // vector<ll> coins(size);
+    // Solving For 3 Coins, can be modified to any 3!
     ll amt;
     cout << "Enter Amount: ";
     cin >> amt;
-    minCoins.resize(amt);
-    // cout << "Enter Values of Coins: ";
-    // for (ll i = 0; i < size; i++)
-    // {
-    //     cin >> coins[i];
-    //     minCoins[coins[i]] = 1;
-    // }
 
-    cout << "Answer is: " << solve(amt);
+    minCoins.assign(amt + 1, -1);
+    minCoins[0] = 0;
+
+    ll result = solve(amt);
+    if (result == INT_MAX) {
+        cout << "Impossible To Obtain!";
+    } else {
+        cout << "Answer is: " << result;
+    }
     return 0;
 }
