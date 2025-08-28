1cce4b5c5cc36b52646217c0b31385bf0bd24279
Shukl-amber <shukl.ember@gmail.com>
2025-08-28 23:14:35 +0530

    Solved

--- CPP/coin_change_leet.cpp
+++ CPP/coin_change_leet.cpp
@@ -1,46 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll coin_1 = 3, coin_2 = 5, coin_3 = 7;
-vector<ll> minCoins;
 
-ll solve(ll amt)
+class Solution
 {
-    if (amt < 0)
-        return INT_MAX;
+    vector<ll> minCoins;
 
-    if (amt == 0)
-        return 0;
+public:
+    int coinChange(vector<int> &coins, int amt)
+    {
+        minCoins.assign(amt + 1, -1);
+        minCoins[0] = 0;
 
-    if (minCoins[amt] != -1)
-        return minCoins[amt];
-    
-    ll minResult = min({solve(amt - coin_1), solve(amt - coin_2), solve(amt - coin_3)});
-    
-    if (minResult == INT_MAX) {
-        minCoins[amt] = INT_MAX;
-        return INT_MAX;
-    } else {
-        minCoins[amt] = minResult + 1;
-        return minResult + 1;
+        ll result = solve(coins, amt);
+        if (result == INT_MAX)
+            return -1;
+        else
+            return result;
     }
-}
 
-int main()
-{
-    // Solving For 3 Coins, can be modified to any 3!
-    ll amt;
-    cout << "Enter Amount: ";
-    cin >> amt;
+    ll solve(vector<int> &coins, ll amt)
+    {
+        if (amt < 0)
+            return INT_MAX;
 
-    minCoins.assign(amt + 1, -1);
-    minCoins[0] = 0;
+        if (amt == 0)
+            return 0;
 
-    ll result = solve(amt);
-    if (result == INT_MAX) {
-        cout << "Impossible To Obtain!";
-    } else {
-        cout << "Answer is: " << result;
+        if (minCoins[amt] != -1)
+            return minCoins[amt];
+
+        ll minResult = INT_MAX;
+        ll temp = INT_MAX;
+
+        for (auto i : coins)
+        {
+            temp = solve(coins, amt - i);
+            if (temp < minResult)
+                minResult = temp;
+        }
+
+        if (minResult == INT_MAX)
+        {
+            minCoins[amt] = INT_MAX;
+            return INT_MAX;
+        }
+        else
+        {
+            minCoins[amt] = minResult + 1;
+            return minResult + 1;
+        }
     }
-    return 0;
-}
+};
