7c393ce984973248909dc984a292e2180bd05c32
Amber Shukla <ember.shukl@gmail.com>
2025-09-03 20:02:31 +0530

    CC

---
+++ CPP/CP(CPP)/endless_play.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    int day, hrs;
+    cin >> day >> hrs;
+    cout << ((day - 4) * 24) + hrs << endl;
+    return 0;
+}
