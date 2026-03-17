f47a71383f23b6ba648d3df63c046d9c5d9c0dae
Shukl-amber <shukl.ember@gmail.com>
2026-03-18 00:07:37 +0530

    9

---
+++ CPP/Leetcode/M1/1523.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        if(!(high % 2) && !(low % 2))
+            return (high - low) / 2;
+        return ((high - low) / 2) + 1;
+    }
+};
