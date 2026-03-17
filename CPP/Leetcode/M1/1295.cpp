adbae819f3a381d54507e066a0d16b712eb7833c
Shukl-amber <shukl.ember@gmail.com>
2026-03-18 00:14:15 +0530

    10

--- CPP/Leetcode/M1/1295.cpp
+++ CPP/Leetcode/M1/1295.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int findNumbers(vector<int>& nums) {
         int count = 0;
-        for(auto e : nums)
-            count = (((int)log10(e) + 1) % 2) ? count : count + 1;
+        for(auto i : nums)
+            count = (((int)log10(i) + 1) % 2) ? count : count + 1;
         return count;
     }
 };
