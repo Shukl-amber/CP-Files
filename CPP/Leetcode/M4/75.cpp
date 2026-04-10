1c680a54c197177460c95efb4b7a38631e847dd5
Shukl-amber <shukl.ember@gmail.com>
2026-04-11 00:00:53 +0530

    6

---
+++ CPP/Leetcode/M4/75.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int l = 0, mid = 0, r = (int)nums.size() - 1;
+        while (mid <= r)
+        {
+            if (nums[mid] == 0)
+            {
+                swap(nums[l], nums[mid]);
+                l++;
+                mid++;
+            }
+            else if (nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums[mid], nums[r]);
+                r--;
+            }
+        }
+    }
+};
