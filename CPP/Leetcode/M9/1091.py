5df472db040663ceb11e7ee210e783459c6644ef
Shukl-amber <shukl.ember@gmail.com>
2026-04-27 01:14:41 +0530

    7

---
+++ CPP/Leetcode/M9/1091.py
@@ -0,0 +1,22 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        if grid[0][0] == 1 or grid[n - 1][n - 1] == 1:
+            return -1
+        queue = deque([(0, 0, 1)])
+        visited = {(0, 0)}
+        dirs = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        while queue:
+            r, c, dist = queue.popleft()
+            if r == n - 1 and c == n - 1:
+                return dist
+            for dr, dc in dirs:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] == 0 and (nr, nc) not in visited:
+                    visited.add((nr, nc))
+                    queue.append((nr, nc, dist + 1))
+        return -1
