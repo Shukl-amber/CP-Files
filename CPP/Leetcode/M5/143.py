a6e37f3522e07e616e4e16ac6a2b888a0d116d76
Shukl-amber <shukl.ember@gmail.com>
2026-04-27 00:39:05 +0530

    8

---
+++ CPP/Leetcode/M5/143.py
@@ -0,0 +1,22 @@
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        prev, cur = None, slow.next
+        slow.next = None
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first = tmp1
+            second = tmp2
