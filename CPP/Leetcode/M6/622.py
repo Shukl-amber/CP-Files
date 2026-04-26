e5d088ce030e3fea850630a0182a7c6a88c68ae3
Shukl-amber <shukl.ember@gmail.com>
2026-04-27 00:50:22 +0530

    8

---
+++ CPP/Leetcode/M6/622.py
@@ -0,0 +1,34 @@
+class MyCircularQueue:
+    def __init__(self, k: int):
+        self.queue = [0] * k
+        self.head = 0
+        self.tail = 0
+        self.size = 0
+        self.capacity = k
+
+    def enQueue(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.queue[self.tail] = value
+        self.tail = (self.tail + 1) % self.capacity
+        self.size += 1
+        return True
+
+    def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.head = (self.head + 1) % self.capacity
+        self.size -= 1
+        return True
+
+    def Front(self) -> int:
+        return -1 if self.isEmpty() else self.queue[self.head]
+
+    def Rear(self) -> int:
+        return -1 if self.isEmpty() else self.queue[(self.tail - 1) % self.capacity]
+
+    def isEmpty(self) -> bool:
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        return self.size == self.capacity
