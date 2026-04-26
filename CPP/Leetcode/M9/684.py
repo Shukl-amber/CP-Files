22454d9294497d9acb9fe6395245746e41687697
Shukl-amber <shukl.ember@gmail.com>
2026-04-27 01:14:19 +0530

    2

---
+++ CPP/Leetcode/M9/684.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        parent = list(range(len(edges) + 1))
+
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+
+        def union(x, y):
+            px, py = find(x), find(y)
+            if px == py:
+                return False
+            parent[px] = py
+            return True
+
+        for u, v in edges:
+            if not union(u, v):
+                return [u, v]
