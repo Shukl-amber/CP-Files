0b6184c1dfafd4afce661b70dfc295864ba32a7f
Shukl-amber <shukl.ember@gmail.com>
2026-04-27 01:14:41 +0530

    9

---
+++ CPP/Leetcode/M9/1584.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        edges = []
+        for i in range(n):
+            for j in range(i + 1, n):
+                dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+                edges.append((dist, i, j))
+        edges.sort()
+        parent = list(range(n))
+
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+
+        cost = 0
+        for w, u, v in edges:
+            pu, pv = find(u), find(v)
+            if pu != pv:
+                parent[pu] = pv
+                cost += w
+        return cost
