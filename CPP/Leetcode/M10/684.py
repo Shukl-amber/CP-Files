07c683633d1ad9a360e1b6a8635bef6fb78f97fe
Shukl-amber <shukl.ember@gmail.com>
2026-04-27 01:19:37 +0530

    7

---
+++ CPP/Leetcode/M10/684.py
@@ -0,0 +1,20 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        px, py = self.find(x), self.find(y)
+        if px == py:
+            return False
+        if self.rank[px] < self.rank[py]:
+            px, py = py, px
+        self.parent[py] = px
+        if self.rank[px] == self.rank[py]:
+            self.rank[px] += 1
+        return True
